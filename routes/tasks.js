8d3098edb23a91c932e3790ae8a926b36191cdff
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-10 19:54:50 +0400

    implement update crud operation in task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -31,13 +31,36 @@ async function handleTasksRoutes(req, res) {
         })
       );
     });
+  } else if (req.method === "PUT" && req.url.startsWith("/tasks/")) {
+    const taskId = req.url.split("/tasks/")[1];
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", async () => {
+      const updates = JSON.parse(body);
+      const result = await taskCollection.updateOne(
+        { _id: new ObjectId(taskId) },
+        { $set: updates }
+      );
+      if (result.matchedCount === 0) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Task not found" }));
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Task updated successfully" }));
+      }
+    });
   } else if (req.method === "DELETE" && req.url.startsWith("/tasks/")) {
     const taskId = req.url.split("/tasks/")[1];
-    const result = await taskCollection.deleteOne({ _id: new ObjectId(taskId) });
-    if(result.deletedCount === 0){
+    const result = await taskCollection.deleteOne({
+      _id: new ObjectId(taskId),
+    });
+    if (result.deletedCount === 0) {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Task not found" }));
-    }else{
+    } else {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Task deleted successfully" }));
     }
